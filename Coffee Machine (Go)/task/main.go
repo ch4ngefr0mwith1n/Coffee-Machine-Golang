ae67e5b9c1c653bb73eeeea77419ea30e2511fc5
Lazar <baldur059@gmail.com>
2023-05-21 23:38:03 +0200

    fourth phase done

--- Coffee Machine (Go)/task/main.go
+++ Coffee Machine (Go)/task/main.go
@@ -2,26 +2,22 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	drink := Drink{
-		water:       200,
-		milk:        50,
-		coffeeBeans: 15,
-	}
-
 	coffeeMachine := CoffeeMachine{}
-	coffeeMachine.fillMachine()
+	coffeeMachine.fillMachine(400, 540, 120, 9, 550)
 
-	coffeeMachine.howManyCups(drink)
+	coffeeMachine.showState()
+	coffeeMachine.actionMenu()
+	coffeeMachine.showState()
 }
 
 type Drink struct {
 	water       int
 	milk        int
 	coffeeBeans int
+	price       int
 }
 
 type CoffeeMachine struct {
@@ -29,40 +25,109 @@ type CoffeeMachine struct {
 	availableMilk        int
 	availableCoffeeBeans int
 	availableCups        int
+	availableMoney       int
 }
 
-func (coffeeMachine *CoffeeMachine) fillMachine() {
-	var water, milk, coffee int
-
-	fmt.Println("Write how many ml of water the coffee machine has:")
-	_, _ = fmt.Scan(&water)
-
-	fmt.Println("Write how many ml of milk the coffee machine has:")
-	_, _ = fmt.Scan(&milk)
-
-	fmt.Println("Write how many grams of coffee beans the coffee machine has:")
-	_, _ = fmt.Scan(&coffee)
-
+func (coffeeMachine *CoffeeMachine) fillMachine(water int, milk int, coffeeBeans int, disposableCups int, money int) {
 	coffeeMachine.availableWater = water
 	coffeeMachine.availableMilk = milk
-	coffeeMachine.availableCoffeeBeans = coffee
+	coffeeMachine.availableCoffeeBeans = coffeeBeans
+	coffeeMachine.availableCups = disposableCups
+	coffeeMachine.availableMoney = money
 }
 
-func (coffeeMachine *CoffeeMachine) howManyCups(drink Drink) {
-	fmt.Println("Write how many cups of coffee you will need:")
+func (coffeeMachine *CoffeeMachine) showState() {
+	fmt.Printf("The coffee machine has:\n%d ml of water\n%d ml of milk\n%d g of coffee beans\n%d disposable cups\n$%d of money\n\n", coffeeMachine.availableWater, coffeeMachine.availableMilk, coffeeMachine.availableCoffeeBeans, coffeeMachine.availableCups, coffeeMachine.availableMoney)
+}
 
-	var neededCups int
-	_, _ = fmt.Scan(&neededCups)
+func (coffeeMachine *CoffeeMachine) actionMenu() {
+	fmt.Println("Write action (buy, fill, take):")
+	var action string
+	fmt.Scan(&action)
 
-	cups := int(
-		math.Min(
-			float64(coffeeMachine.availableWater/drink.water), math.Min(float64(coffeeMachine.availableMilk/drink.milk), float64(coffeeMachine.availableCoffeeBeans/drink.coffeeBeans))))
-
-	if cups == neededCups {
-		fmt.Println("Yes, I can make that amount of coffee")
-	} else if cups > neededCups {
-		fmt.Printf("Yes, I can make that amount of coffee (and even %d more than that)\n", cups-neededCups)
-	} else if cups < neededCups {
-		fmt.Printf("No, I can make only %d cups of coffee", cups)
+	switch action {
+	case "buy":
+		coffeeMachine.buyCoffee()
+		break
+	case "fill":
+		coffeeMachine.fillMenu()
+		break
+	case "take":
+		coffeeMachine.takeMoney()
+		break
 	}
+}
+
+func (coffeeMachine *CoffeeMachine) takeMoney() {
+	fmt.Printf("I gave you $%d\n", coffeeMachine.availableMoney)
+	coffeeMachine.availableMoney = 0
+}
+
+func (coffeeMachine *CoffeeMachine) buyCoffee() {
+	fmt.Println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+	var option int
+	fmt.Scan(&option)
+
+	switch option {
+	case 1:
+		espresso := Drink{
+			water:       250,
+			milk:        0,
+			coffeeBeans: 16,
+			price:       4,
+		}
+
+		coffeeMachine.makeTransaction(espresso)
+		break
+	case 2:
+		latte := Drink{
+			water:       350,
+			milk:        75,
+			coffeeBeans: 20,
+			price:       7,
+		}
+
+		coffeeMachine.makeTransaction(latte)
+		break
+	case 3:
+		cappuccino := Drink{
+			water:       200,
+			milk:        100,
+			coffeeBeans: 12,
+			price:       6,
+		}
+
+		coffeeMachine.makeTransaction(cappuccino)
+		break
+	}
+}
+
+func (coffeeMachine *CoffeeMachine) fillMenu() {
+	var water, milk, coffee, disposableCups int
+
+	fmt.Println("Write how many ml of water you want to add:")
+	_, _ = fmt.Scan(&water)
+
+	fmt.Println("Write how many ml of milk you want to add:")
+	_, _ = fmt.Scan(&milk)
+
+	fmt.Println("Write how many grams of coffee beans you want to add:")
+	_, _ = fmt.Scan(&coffee)
+
+	fmt.Println("Write how many disposable cups you want to add:")
+	_, _ = fmt.Scan(&disposableCups)
+
+	coffeeMachine.availableWater += water
+	coffeeMachine.availableMilk += milk
+	coffeeMachine.availableCoffeeBeans += coffee
+	coffeeMachine.availableCups += disposableCups
+}
+
+func (coffeeMachine *CoffeeMachine) makeTransaction(drink Drink) {
+	coffeeMachine.availableWater -= drink.water
+	coffeeMachine.availableMilk -= drink.milk
+	coffeeMachine.availableCoffeeBeans -= drink.coffeeBeans
+	coffeeMachine.availableCups -= 1
+
+	coffeeMachine.availableMoney += drink.price
 }
